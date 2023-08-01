f71184d24f436d71c74b4cfeb0688133abe21bdc
Alanoud900 <140309909+Alanoud900@users.noreply.github.com>
2023-08-01 09:44:50 +0300

    Add files via upload

---
+++ week 2 day 2 (Lap2)/week 2 day 2 (Lap2)/week_2_day_2__Lap2_App.swift
@@ -0,0 +1,17 @@
+//
+//  week_2_day_2__Lap2_App.swift
+//  week 2 day 2 (Lap2)
+//
+//  Created by Alanoud  on 13/01/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct week_2_day_2__Lap2_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
